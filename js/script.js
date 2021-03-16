862c41e0956844aa605c38cf3159d6e51594dfaf
econom1st <camcycamovu4@gmail.com>
2021-03-16 08:27:47 +0300

    add in moodal setTimeout and docElement

--- js/script.js
+++ js/script.js
@@ -152,13 +152,19 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           closeModalBtn = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
+
+  
 
     function closeModal() {
         modal.classList.add('hide');
@@ -179,6 +185,17 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 
 
 });
