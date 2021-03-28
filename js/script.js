38f5013639aeabd669932225263625478bfb5f4d
econom1st <camcycamovu4@gmail.com>
2021-03-28 12:18:29 +0300

    changed to fetch FORMDATA

--- js/script.js
+++ js/script.js
@@ -299,35 +299,33 @@ window.addEventListener('DOMContentLoaded', () => {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-
-            const request = new XMLHttpRequest();
-
-            request.open('POST', 'server.php');
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
-            request.setRequestHeader('Content-type', 'application/json');
-
+  
             const formData = new FormData(form);
             
-            const object = {};
-            formData.forEach(function(value,key) {
-                object[key] = value;
+            // const object = {};
+            // formData.forEach(function(value,key) {
+            //     object[key] = value;
+            // });
+
+            // const json = JSON.stringify(object);
+
+            fetch('server.php', {
+                method: 'POST',
+                // headers: {
+                //     'Content-type': 'application/json'
+                // },
+                body: formData
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.fail);
+            }).finally(() => {
+                form.reset();
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-
-                } else {
-                    showThanksModal(message.fail);
-                }
-            });
         });
     }
 
@@ -356,7 +354,19 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    // fetch('https://jsonplaceholder.typicode.com/todos/1') // возвращается promise
+    // .then(response => response.json())
+    // .then(json => console.log(json));
 
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: 'POST',
+    //     body: JSON.stringify({name: 'Alex'}),
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     } 
+    // }) 
+    // .then(response => response.json())
+    // .then(json => console.log(json));
 
 
 
